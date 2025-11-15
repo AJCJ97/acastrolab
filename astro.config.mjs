aab4724c6dac9f801ce83daa2bdc19669e5cc603
ACastro <ajcastro9726@gmail.com>
2025-11-15 01:18:41 -0600

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
 
 export default defineConfig({
     site: 'https://ajcj97.github.io/', 
