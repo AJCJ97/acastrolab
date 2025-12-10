e85ca049471399b06969acbb669e9872110162d3
ACastro <ajcastro9726@gmail.com>
2025-12-09 20:10:03 -0600

    logo added

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,22 +5,13 @@ import react from '@astrojs/react';
 
 export default defineConfig({
     site: 'https://ajcj97.github.io/',
-    // 1. **CLAVE:** Mantén base con el nombre exacto de tu repo
+    
     base: '/acanalytics', 
     outDir: './dist',
     trailingSlash: 'always',
     output: 'static',
-
-    // 2. ELIMINA/COMENTA CUALQUIER BLOQUE 'build' anterior
-    // build: {
-    //    assetsPrefix: 'relative', // ¡Eliminar o comentar!
-    // },
-
-    // 3. Usa la configuración de Vite SIN un 'base' explícito aquí, 
-    //    ya que puede entrar en conflicto con el 'base' de Astro.
     vite: {
         plugins: [tailwindcss()],
-        // base: '/acanalytics/', // ¡Eliminar o comentar esta línea!
     },
     
     integrations: [react()],
